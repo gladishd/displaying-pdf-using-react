59897bbf1e81ee4d4afc00eb6c2a71d500f6ce92
gladishd <gladish.dean@gmail.com>
2022-12-30 17:02:36 -0600

    import samplePDF1 from ./mangroveBackstory.pdf; import samplePDF2 from ./qooleyBackstory.pdf; and displays pdf={samplePDF2} and pdf={samplePDF1} from the App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from "react";
 import SinglePagePDFViewer from "./components/pdf/single-page";
 import AllPagesPDFViewer from "./components/pdf/all-pages";
 
-/* This is required only if the project file is located 
+/* This is required only if the project file is located
 inside the app. Otherwise you can use the external link of the pdf file*/
-import samplePDF from "./sample.pdf";
+import samplePDF1 from "./mangroveBackstory.pdf";
+import samplePDF2 from "./qooleyBackstory.pdf";
 
 import "./styles.css";
 
@@ -13,13 +14,24 @@ export default function App() {
   return (
     <div className="App">
       <h4>Single Page</h4>
-      <SinglePagePDFViewer pdf={samplePDF} />
+      <SinglePagePDFViewer pdf={samplePDF1} />
 
       <hr />
 
       <h4>All Pages</h4>
       <div className="all-page-container">
-        <AllPagesPDFViewer pdf={samplePDF} />
+        <AllPagesPDFViewer pdf={samplePDF1} />
+      </div>
+
+      <hr />
+      <h4>Single Page</h4>
+      <SinglePagePDFViewer pdf={samplePDF2} />
+
+      <hr />
+
+      <h4>All Pages</h4>
+      <div className="all-page-container">
+        <AllPagesPDFViewer pdf={samplePDF2} />
       </div>
 
       <hr />
